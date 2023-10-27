cafe233dae7771c5f6e912a5cf893a1867b9ddc1
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-26 21:23:44 -0700

    this is sum of two integer number through the scanner class

---
+++ ReadData.java
@@ -0,0 +1,14 @@
+package java_file_handling;
+import java.util.*;
+public class ReadData {
+    public static void main(String []arr){
+        Scanner sc = new Scanner(System.in);
+        int i,j,k;
+        System.out.println("Enter value of i:");
+        i=sc.nextInt();
+        System.out.println("Enter value of j:");
+        j=sc.nextInt();
+        k=i+j;
+        System.out.println(k);
+    }
+}
