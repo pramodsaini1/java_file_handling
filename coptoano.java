6b61715a728af85684f47d4e2448294b9f5e93d0
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-27 20:46:05 -0700

    this is copy from one file and paste is another file

---
+++ coptoano.java
@@ -0,0 +1,24 @@
+package java_file_handling;
+import java.io.*;
+public class coptoano {
+    public static void main(String []arr) throws IOException{
+         try{
+               FileInputStream fr = new FileInputStream("WriteData");
+               FileOutputStream fw = new FileOutputStream("pk.txt");
+               PrintStream out=new PrintStream(fw);
+               int i;
+               while(true){
+                  i=fr.read();
+                  if(i==-1){
+                    break;
+                  }
+                  out.print((char)i);
+               }
+               fr.close();
+               fw.close();
+         }
+         catch(FileNotFoundException e){
+            System.out.println("File Not Found");
+         }
+    }
+}
