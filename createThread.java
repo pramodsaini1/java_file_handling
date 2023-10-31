68f6fd2c02073e7e9d8595d5de1f06e79d06922c
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-30 20:34:29 -0700

    how to create the thread in java

---
+++ createThread.java
@@ -0,0 +1,8 @@
+package java_file_handling;
+
+public class createThread {
+    public static void main(String []arr){
+        Thread t = Thread.currentThread();
+        System.out.println(t);
+    }
+}
